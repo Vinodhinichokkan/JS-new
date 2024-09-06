f4e7df1860165950c277633d7526e03e1da4a75e
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-09-06 12:28:28 +0530

    exe

---
+++ forloop.js
@@ -0,0 +1,26 @@
+for (i=1;i<=10;i++)
+{
+    console.log(i);
+}                          //1 2 3 4 5 6 7 8 9 10
+
+
+let name="Vinodhini"
+for (i=0;i<=name.length;i=i+2)
+{
+    console.log(name.charAt(i));   //V n d i i
+}
+
+
+let player="Vinodhini"
+for (i=0;i<=player.length;i=i+2)
+{
+    for (j=0;j<=player.length;j=j+2)
+    {
+
+    console.log(player.charAt(i)); 
+    }                                      //
+}
+
+
+
+ 
