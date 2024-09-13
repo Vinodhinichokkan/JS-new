642eb44ba6154415a55ac0a5a8655ad6fa21124d
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-09-13 11:02:29 +0530

    exe

--- functions.js
+++ functions.js
@@ -1,3 +1,39 @@
 console.log("Vinodhini".toUpperCase());  //VINODHINI
 
-console.log("Vinodhini".toLowerCase()); //
+console.log("Vinodhini".toLowerCase()); //vinodhini
+
+function sum (num1,num2){
+    console.log(num1);
+    console.log(num2);
+    return num1+num2;
+}
+
+console.log(sum(10));  // 10  undefined  NAN
+
+
+function sum (num1,num2){
+    console.log(num1);
+    console.log(num2);
+    if(num2===undefined){
+        return num1+num1;
+    }
+    return num1+num2;
+}
+
+console.log(sum(10));     // 10  undefined  20 
+
+
+//---------------slice method--------------//
+email="vinodhini1999@gmail.com"
+console.log(email.slice(0,13));
+
+              (or)
+
+console.log(email.slice(0,email.indexOf("@")));  
+
+             (or)
+function getUserNameFromEmail(email){
+    return(email.slice(0,email.indexOf("@")));
+}
+
+console.log(getUserNameFromEmail("vinodhini1999@gmail.com"));
