2391b9e82d35bff75926ac31d6754b2612537f08
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-09-18 13:29:53 +0530

    exe

--- keyvalue.js
+++ keyvalue.js
@@ -1,5 +1,63 @@
 //key value pairs
+//obj
 
-const myObj = {name:"vinodhini"};
+const myObj = {name:"Vinodhini"};
 
-console.log(myObj.name);
+const anotherObj = {
+    subscriber:true,
+    no: 10000,
+    content:{
+        earn:"JS",
+        grow:"stocks"
+    },
+    subdivisions: ["earn","Grow","Give"]
+};
+
+console.log(myObj.name); 
+console.log(anotherObj.content.grow);
+
+
+            //-----(or)---------//
+
+
+const myValue = {name:"Vinodhini"};
+
+const anotherValue = {
+    subscriber:true,
+    no: 10000,
+    content:{
+        earn:"JS",
+        grow:"stocks"
+    },
+     subdivisions: ["earn","Grow","Give"]
+};
+            
+console.log(myValue.name); 
+console.log(anotherValue["subscriber"]);
+
+
+//-----------(or)----------//
+  
+
+const myConst = {name:"Vinodhini"};
+
+const anotherConst = {
+    subscriber:true,
+    no: 10000,
+    content:{
+        earn:"JS",
+        grow:"stocks"
+    },
+     subdivisions: ["earn","Grow","Give"],
+     action: function(){
+        return `Earn by learning ${
+            this.content.earn}`;
+        
+     }
+};
+            
+console.log(myConst.name); 
+console.log(anotherConst["subscriber"]);
+
+
+            
